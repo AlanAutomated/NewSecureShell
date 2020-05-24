52efab3a4120d82dfebb19ba87d004025d61968b
Alan Haynes <alan@networkautomation.engineer>
2020-05-24 11:50:32 -0400

    another updat

--- NewSecureShell.psm1
+++ NewSecureShell.psm1
@@ -30,4 +30,4 @@ $ScriptBlock = {
 Register-ArgumentCompleter -CommandName New-SecureShell -ParameterName SessionHost -ScriptBlock $ScriptBlock
 
 # Finally, export the function
-Export-ModuleMember -Function New-SecureShell
+Export-ModuleMember -Function *
